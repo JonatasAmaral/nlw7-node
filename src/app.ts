e38d57f5b136b0d62564405fa9ce3367d2b36b96
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-19 00:00:50 -0300

    create GITHUB oauth login

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 const port = 4000;
 
+app.get("/github", (request, response) => {
+	response.redirect(
+		`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+	);
+});
+app.get("/signin/callback", (request, response) => {
+	const { code } = request.query;
+
+	return response.json(code);
+});
 app.listen(port, () => console.log("🚀 Server running on PORT " + port));
