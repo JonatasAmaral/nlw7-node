71740c087a68784099b660fc04d12be5206ad4d8
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-19 23:50:35 -0300

    get user access_token data

--- src/Services/AuthenticateUserService.ts
+++ src/Services/AuthenticateUserService.ts
@@ -1,21 +1,26 @@
 import axios from "axios";
 import { response } from "express";
 
+interface IAccessTokenResponse {
+	access_token: string;
+}
+
 class AuthenticateUserService {
 	async execute(code: string) {
 		const url = "https://github.com/login/oauth/access_token";
 
-		const response = await axios.post(url, null, {
-			params: {
-				client_id: process.env.GITHUB_CLIENT_ID,
-				client_secret: process.env.GITHUB_CLIENT_SECRET,
-				code,
-			},
-			headers: {
-				Accept: "application/json",
-			},
-		});
-		return response.data;
+		const { data: accessTokenResponse } =
+			await axios.post<IAccessTokenResponse>(url, null, {
+				params: {
+					client_id: process.env.GITHUB_CLIENT_ID,
+					client_secret: process.env.GITHUB_CLIENT_SECRET,
+					code,
+				},
+				headers: {
+					Accept: "application/json",
+				},
+			});
+		return accessTokenResponse;
 	}
 }
 
