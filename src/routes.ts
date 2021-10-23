5692fbdf0254be3cabe36ea053390a2978fba324
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-23 09:47:08 -0300

    ensure user is authenticaded to send messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { AuthenticateUserControler } from "./Controllers/AuthenticateUserControler";
+import { CreateMessageController } from "./Controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserControler().handle);
+router.post(
+	"/messages",
+	ensureAuthenticated,
+	new CreateMessageController().handle
+);
 
 export { router };
