f3029b358a024cd723ff3d97efecbd22372afe26
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-23 16:00:02 -0300

    create a "get many massages" url

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateUserControler } from "./Controllers/AuthenticateUserControl
 import { CreateMessageController } from "./Controllers/CreateMessageController";
 import { GetLastMessagesController } from "./Controllers/GetLastMessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { ProfileUserController } from "./Controllers/ProfileUserController";
 
 const router = Router();
 
@@ -12,10 +13,16 @@ router.post(
 	ensureAuthenticated,
 	new CreateMessageController().handle
 );
-console.log("construir");
 
 router.get("/messages/last3", (req, res) => {
 	new GetLastMessagesController().handle(req, res, 3);
+});
+router.get("/messages/last", (req, res) => {
+	new GetLastMessagesController().handle(
+		req,
+		res,
+		parseInt(req.query.amount as string) || undefined
+	);
 });
 
 router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
